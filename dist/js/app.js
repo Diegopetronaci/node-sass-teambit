b88534ff4c76a792d6c9819540dcde6908d9ca23
Diego <Petronacidiego@gmail.com>
2021-01-26 16:53:49 +0100

    commit iniziale con nav creata

---
+++ dist/js/app.js
@@ -0,0 +1,17 @@
+/******/ (() => { // webpackBootstrap
+(() => {
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+
+})();
+
+(() => {
+/*!**********************!*\
+  !*** ./src/app.scss ***!
+  \**********************/
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Undefined variable.\n   ╷\n26 │                 color: $link;\r\n   │                        ^^^^^\n   ╵\n  src\\partials\\_nav.scss 26:24  @import\n  C:\\Users\\Diego\\Desktop\\Corso Boolean\\consegne\\mese 4\\node-sass-teambit\\src\\app.scss 2:9                     root stylesheet\n    at processResult (C:\\Users\\Diego\\Desktop\\Corso Boolean\\consegne\\mese 4\\node-sass-teambit\\node_modules\\webpack\\lib\\NormalModule.js:597:19)\n    at C:\\Users\\Diego\\Desktop\\Corso Boolean\\consegne\\mese 4\\node-sass-teambit\\node_modules\\webpack\\lib\\NormalModule.js:691:5\n    at C:\\Users\\Diego\\Desktop\\Corso Boolean\\consegne\\mese 4\\node-sass-teambit\\node_modules\\loader-runner\\lib\\LoaderRunner.js:399:11\n    at C:\\Users\\Diego\\Desktop\\Corso Boolean\\consegne\\mese 4\\node-sass-teambit\\node_modules\\loader-runner\\lib\\LoaderRunner.js:251:18\n    at context.callback (C:\\Users\\Diego\\Desktop\\Corso Boolean\\consegne\\mese 4\\node-sass-teambit\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at C:\\Users\\Diego\\Desktop\\Corso Boolean\\consegne\\mese 4\\node-sass-teambit\\node_modules\\sass-loader\\dist\\index.js:73:7\n    at Function.call$2 (C:\\Users\\Diego\\Desktop\\Corso Boolean\\consegne\\mese 4\\node-sass-teambit\\node_modules\\sass\\sass.dart.js:91131:16)\n    at _render_closure1.call$2 (C:\\Users\\Diego\\Desktop\\Corso Boolean\\consegne\\mese 4\\node-sass-teambit\\node_modules\\sass\\sass.dart.js:80007:12)\n    at _RootZone.runBinary$3$3 (C:\\Users\\Diego\\Desktop\\Corso Boolean\\consegne\\mese 4\\node-sass-teambit\\node_modules\\sass\\sass.dart.js:27193:18)\n    at _FutureListener.handleError$1 (C:\\Users\\Diego\\Desktop\\Corso Boolean\\consegne\\mese 4\\node-sass-teambit\\node_modules\\sass\\sass.dart.js:25721:19)");
+})();
+
+/******/ })()
+;
